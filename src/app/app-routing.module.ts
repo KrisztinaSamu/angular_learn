348eb1621b88c887b5499622e04419372ee987ab
Kriszti <letina84@gmail.com>
2023-03-26 15:51:52 +0200

    Files upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'myComponent', component: MyComponentComponent },
+  { path: '**', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
