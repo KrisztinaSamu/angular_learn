80878a30d788861c209323e93d468c00203aa166
Kriszti <letina84@gmail.com>
2023-04-17 08:59:18 +0200

    Create component -- comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-an
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { PostCardComponent } from './layouts/post-card/post-card.component';
     TermsAndConditionComponent,
     ContactUsComponent,
     SubscriptionFormComponent,
-    PostCardComponent
+    PostCardComponent,
+    CommentFormComponent
   ],
   imports: [
     BrowserModule,
