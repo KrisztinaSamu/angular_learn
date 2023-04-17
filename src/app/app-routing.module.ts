43d7492e706c77977e9fd690bc2d088fba8d4393
Kriszti <letina84@gmail.com>
2023-04-17 10:31:46 +0200

    ADD DESIGN -- terms and condition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
-import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,8 +12,7 @@ const routes: Routes = [
   { path: 'post', component: SinglePostComponent },
 
   { path: 'term-conditions', component: TermsAndConditionComponent },
-  { path: 'contact', component: ContactUsComponent },
-  { path: 'comments', component: CommentFormComponent }
+  { path: 'contact', component: ContactUsComponent }
 ];
 
 @NgModule({
