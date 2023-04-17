ddc7c9c348b4245c55b549482acb86f06f90742e
Kriszti <letina84@gmail.com>
2023-04-17 09:39:29 +0200

    CREATE and ADD DESIGN -- comment-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'post', component: SinglePostComponent },
 
   { path: 'term-conditions', component: TermsAndConditionComponent },
-  { path: 'contact', component: ContactUsComponent }
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'comments', component: CommentFormComponent }
 ];
 
 @NgModule({
